5fc7c2a88815588b85dde9f68eb56566b7d9ac81
bisidro <bianca.isidro@gmail.com>
2025-06-10 20:04:27 -0700

    Add repo config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type { import("next").NextConfig } */
+const repo = 'bisidro.github.io';
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
