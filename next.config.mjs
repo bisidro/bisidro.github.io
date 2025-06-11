23b7260a40660efdc89e176d2eef1412e94a2187
bisidro <bianca.isidro@gmail.com>
2025-06-10 20:50:51 -0700

    Test

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
-/** @type { import("next").NextConfig } */
-const repo = 'bisidro.github.io';
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`
 };
 
 export default nextConfig;
